c9af78ac95533e146ee39df2ce6ab89647f46241
Rohan <ansarorkhan@gmail.com>
2019-06-18 01:49:55 +0400

    Events complete

--- Events/app/src/main/java/com/example/events/MainActivity.java
+++ Events/app/src/main/java/com/example/events/MainActivity.java
@@ -1,36 +1,66 @@
 package com.example.events;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
+import java.util.Date;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private Random rndm = new Random();
+    private ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.actionBar = getSupportActionBar();
     }
 
     public void clickEvent(View view) {
-        DisplayMetrics parent = this.getResources().getDisplayMetrics();
-        Button btn = (Button)view;
-        if(view.getWidth() < parent.widthPixels && view.getHeight() < parent.heightPixels) {
-            view.setMinimumWidth(view.getWidth() + 50);
-            view.setMinimumHeight(view.getHeight() + 50);
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(
+                new ColorDrawable(
+                    Color.rgb(rndm.nextInt(256),
+                                rndm.nextInt(256),
+                                rndm.nextInt(256)
+                    )
+                )
+            );
+            Date date = new Date();
+            int diff;
+            if(date.getHours() <= 12) {
+                diff = 12 - date.getHours();
+            } else {
+                diff = 36 - date.getHours();
+            }
+            int minutes = 0;
+            if(date.getMinutes() != 0) {
+                diff--;
+                minutes = 60 - date.getMinutes();
+            }
+            actionBar.setTitle(diff + ":" + minutes);
+        }
+        if(count < 20) {
+            view.setMinimumWidth(view.getWidth() + 20);
+            view.setMinimumHeight(view.getHeight() + 20);
+            view.setBackgroundColor(Color.rgb(rndm.nextInt(256), rndm.nextInt(256), rndm.nextInt(256)));
+            count++;
+            ((Button) view).setText(count + "");
+            ((Button) view).setTextSize(rndm.nextInt(23) + 13);
+            ((Button) view).setTextColor(Color.rgb(rndm.nextInt(256), rndm.nextInt(256), rndm.nextInt(256)));
         } else {
-            btn.setText(R.string.bigger_button_text);
-            btn.setTextColor(Color.RED);
-            btn.setTextSize(36);
-            btn.setEnabled(false);
+            ((Button) view).setText(R.string.bigger_button_text);
+            ((Button) view).setTextColor(Color.RED);
+            ((Button) view).setTextSize(36);
+            ((Button) view).setEnabled(false);
         }
     }
 }
